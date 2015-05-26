6b9372081d31aa9ce631c2046992a4996d38b0e3
kresowe <michal.barej@yahoo.com>
2015-05-26 23:36:55 +0200

    Some unittests for Disc. In Mallet class, PyCharm itself underlines some lines as warnings.

--- data/tests/TestDisc.py
+++ data/tests/TestDisc.py
@@ -1,62 +1,57 @@
 from __future__ import absolute_import
 import unittest
 import math
-from data import Disc
+#from data.Disc import Disc
+from DummyDisc import DummyDisc
+from data.Vector import Vector
 
 
 class TestDisc(unittest.TestCase):
-    expected_points = (((0, 0), (1, 2), (1, 2)),
-                       ((0, 0), (1, -2), (1, -2)),
-                       ((1, -1), (1, 2), (2, 1)),
-                       ((-5, 3), (-2, -1), (-7, 2)))
+    expected_points = (((100, 100), (11, 20), (111, 120)),
+                       ((100, 100), (11, -20), (111, 80)),
+                       ((101, 99), (10, 21), (111, 120)),
+                       ((95, 103), (-25, -15), (70, 88)))
 
-    expected_points_to = (((0, 0), (1, 2), (1, 2)),
-                          ((0, 0), (1, -2), (1, -2)),
-                          ((1, -1), (1, 2), (1, 2)),
-                          ((-5, 3), (-2, -1), (-2, -1)))
+    expected_points_to = (((100, 100), (111, 120), (111, 120)),
+                          ((100, 100), (111, 80), (111, 80)),
+                          ((100, 90), (111, 120), (111, 120)),
+                          ((70, 130), (70, 88), (70, 88)))
 
     expected_velocity_value = (((0, 0), 0),
                                ((1, -1), math.sqrt(2)),
                                ((1, 2), math.sqrt(5)),
                                ((100, 80), math.sqrt(16400)))
 
+    #we don't need to check whether disc is on pitch. It's a task of border_colision
     def test_move_to(self):
         for tup in self.expected_points_to:
-            t_disc = Disc.Disc()
-            t_disc.moveTo(tup[0][0], tup[0][1])
-            t_disc.moveTo(tup[1][0], tup[1][1])
-            result = t_disc.position.x, t_disc.position.y
+            t_disc = DummyDisc(tup[0][0], tup[0][1], 1, 26, [(0, 800), (0, 600)])
+            t_disc.move_to(tup[1][0], tup[1][1])
+            result = t_disc.pos.x, t_disc.pos.y
             self.assertEqual(tup[2], result)
 
     def test_move(self):
         for tup in self.expected_points:
-            t_disc = Disc.Disc()
-            t_disc.moveTo(tup[0][0], tup[0][1])
+            t_disc = DummyDisc(tup[0][0], tup[0][1], 1, 26, [(0, 800), (0, 600)])
             t_disc.move(tup[1][0], tup[1][1])
-            result = t_disc.position.x, t_disc.position.y
+            result = t_disc.pos.x, t_disc.pos.y
             self.assertEqual(tup[2], result)
 
     def test_velocity_set(self):
         for tup in self.expected_points_to:
-            t_disc = Disc.Disc()
-            t_disc.velocity = tup[0][0], tup[0][1]
-            t_disc.velocity = tup[1][0], tup[1][1]
-            result = t_disc.velocity.v_x, t_disc.velocity.v_y
+            t_disc = DummyDisc(100, 100, 1, 26, [(0, 800), (0, 600)])
+            t_disc.vel = Vector(tup[0][0], tup[0][1])
+            t_disc.vel = Vector(tup[1][0], tup[1][1])
+            result = t_disc.vel.x, t_disc.vel.y
             self.assertEqual(tup[2], result)
 
     def test_accelerate(self):
         for tup in self.expected_points:
-            t_disc = Disc.Disc()
-            t_disc.velocity = tup[0][0], tup[0][1]
+            t_disc = DummyDisc(100, 100, 1, 26, [(0, 800), (0, 600)])
+            t_disc.vel = Vector(tup[0][0], tup[0][1])
             t_disc.accelerate(tup[1][0], tup[1][1])
-            result = t_disc.velocity.v_x, t_disc.velocity.v_y
+            result = t_disc.vel.x, t_disc.vel.y
             self.assertEqual(tup[2], result)
-
-
-    def test_print_status(self):
-        t_disc = Disc.Disc()
-        t_disc.velocity = 3, 4
-        t_disc.moveTo(1, 2)
 
 
 
