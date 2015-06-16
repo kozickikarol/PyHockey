de462dea3171403bfe1be2f02cc9c89cbb67f7f5
Karol Kozicki <kozicki.karol0@gmail.com>
2015-06-17 01:53:46 +0200

    Fix camera capture

--- data/VideoCapture.py
+++ data/VideoCapture.py
@@ -11,73 +11,83 @@ class VideoCapture:
         self.player2 = None
         self.data = dict()
         self.frame = None
+        self.VIDEO_SIZE = (400, 300)
 
         self.data[player.player_id] = {
+            'cam_pos': self.player.mallet.pos.state,
             'pos': self.player.mallet.pos.state,
             'last_pos': self.player.mallet.pos.state,
-            'vel': (0, 0)
+            'vel': [(0, 0)]
+            # 'vel': (0, 0)
         }
+        self.set_color_mask(self.player)
         if player2:
             self.player2 = player2
             self.data[player2.player_id] = {
+                'cam_pos': self.player2.mallet.pos.state,
                 'pos': self.player2.mallet.pos.state,
                 'last_pos': self.player2.mallet.pos.state,
-                'vel': (0, 0)
+                # 'vel': (0, 0)
+                'vel': [(0, 0)]
             }
+            self.set_color_mask(self.player2)
 
-        self.set_color_mask()
         self._stop_capture = threading.Event()
         self._stop_image_processing = threading.Event()
 
-    def set_color_mask(self):
-        if self.player.playerColor == Player.PLAYER_BLUE:
-            self.data[self.player.playerColor]['lower'] = np.array([90, 80, 80], dtype=np.uint8)
-            self.data[self.player.playerColor]['upper'] = np.array([110, 255, 255], dtype=np.uint8)
-            self.data[self.player.playerColor]['circle_color'] = (255, 0, 0)
+    def set_color_mask(self, player):
+        if player.playerColor == Player.PLAYER_BLUE:
+            self.data[player.playerColor]['lower'] = np.array([90, 80, 80], dtype=np.uint8)
+            self.data[player.playerColor]['upper'] = np.array([110, 255, 255], dtype=np.uint8)
+            self.data[player.playerColor]['circle_color'] = (255, 0, 0)
         else:
-            self.data[self.player.playerColor]['lower'] = np.array([158, 216, 0], dtype=np.uint8)
-            self.data[self.player.playerColor]['upper'] = np.array([202, 248, 167], dtype=np.uint8)
-            self.data[self.player.playerColor]['circle_color'] = (0, 0, 255)
-        if self.player2:
-            if self.player2.playerColor == Player.PLAYER_BLUE:
-                self.data[self.player2.playerColor]['lower'] = np.array([90, 80, 80], dtype=np.uint8)
-                self.data[self.player2.playerColor]['upper'] = np.array([110, 255, 255], dtype=np.uint8)
-                self.data[self.player2.playerColor]['circle_color'] = (255, 0, 0)
-            else:
-                self.data[self.player2.playerColor]['lower'] = np.array([158, 216, 0], dtype=np.uint8)
-                self.data[self.player2.playerColor]['upper'] = np.array([202, 248, 167], dtype=np.uint8)
-                self.data[self.player2.playerColor]['circle_color'] = (0, 0, 255)
+            self.data[player.playerColor]['lower'] = np.array([21, 58, 28], dtype=np.uint8)
+            self.data[player.playerColor]['upper'] = np.array([105, 224, 154], dtype=np.uint8)
+            self.data[player.playerColor]['circle_color'] = (0, 0, 255)
+
+    def convert_position(self, player_id, tup):
+        GAME_SIZE = (800, 600)
+        # left side
+        if player_id == Player.PLAYER_RED:
+            x = tup[0] * GAME_SIZE[0]/self.VIDEO_SIZE[0]
+            y = tup[1] * GAME_SIZE[1]/self.VIDEO_SIZE[1]
+        # right side
+        else:
+            x = tup[0] * GAME_SIZE[0]/self.VIDEO_SIZE[0] + GAME_SIZE[0]/2
+            y = tup[1] * GAME_SIZE[1]/self.VIDEO_SIZE[1]
+
+        return int(x), int(y)
 
     def get_image(self):
         cap = cv2.VideoCapture(0)
         while not self._stop_capture.is_set():
             _, frame = cap.read()
-            self.frame = cv2.resize(cv2.flip(frame, 1), (800, 600))
+            self.frame = cv2.resize(cv2.flip(frame, 1), self.VIDEO_SIZE)
             for player_id in self.data.keys():
-                cv2.circle(self.frame, self.data[player_id]['pos'], 10, self.data[player_id]['circle_color'], 2)
+                cv2.circle(self.frame, self.data[player_id]['cam_pos'], 10, self.data[player_id]['circle_color'], 2)
             cv2.imshow('frame', self.frame)
-            k = cv2.waitKey(10) & 0xFF
+            k = cv2.waitKey(33) & 0xFF
         cv2.destroyAllWindows()
 
     def get_players_data(self, player_id):
         while not self._stop_image_processing.is_set():
-            frame = self.frame
-            if frame is None:
+
+            if self.frame is None:
                 continue
             if player_id == Player.PLAYER_RED:
-                frame = self.frame[:, :400]
+                frame = self.frame[:, :self.VIDEO_SIZE[0]//2]
             else:
-                frame = self.frame[:, 400:]
+                frame = self.frame[:, self.VIDEO_SIZE[0]//2:]
+            frame = cv2.blur(frame, (3, 3))
             hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
             self.data[player_id]['last_pos'] = self.data[player_id]['pos']
+            if len(self.data[player_id]['vel']) > 4:
+                self.data[player_id]['vel'].pop(0)
             mask = cv2.inRange(hsv, self.data[player_id]['lower'], self.data[player_id]['upper'])
-            element = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
-            mask = cv2.erode(mask, element, iterations=2)
-            mask = cv2.dilate(mask, element, iterations=2)
-            mask = cv2.erode(mask, element)
-            # res = cv2.bitwise_and(frame, frame, mask=mask)
-            # imgray = cv2.medianBlur(cv2.cvtColor(res, cv2.COLOR_BGR2GRAY), 5)
+            kernel = np.ones((5, 5), np.uint8)
+            mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
             mask = cv2.medianBlur(mask, 5)
+
             contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
             maximumArea = 0
             bestContour = None
@@ -86,16 +96,20 @@ class VideoCapture:
                 if currentArea > maximumArea:
                     bestContour = contour
                     maximumArea = currentArea
+
             if bestContour is not None:
-                (x, y), radius = cv2.minEnclosingCircle(bestContour)
-                if player_id == Player.PLAYER_RED:
-                    self.data[player_id]['pos'] = int(x), int(y)
-                    self.data[player_id]['vel'] = (int(x) - self.data[player_id]['last_pos'][0])/10, (int(y) - self.data[player_id]['last_pos'][1])/10
-                else:
-                    self.data[player_id]['pos'] = int(x)+400, int(y)
-                    self.data[player_id]['vel'] = (int(x) - self.data[player_id]['last_pos'][0])/10, (int(y) - self.data[player_id]['last_pos'][1])/10
+                M = cv2.moments(bestContour)
+                x, y = int(M['m10']/M['m00']), int(M['m01']/M['m00'])
+                game_x, game_y = self.convert_position(player_id, (x, y))
+                if player_id == Player.PLAYER_BLUE:
+                    x += self.VIDEO_SIZE[0]//2
+                self.data[player_id]['cam_pos'] = (x, y)
+                self.data[player_id]['pos'] = game_x, game_y
+                self.data[player_id]['vel'].append(((game_x - self.data[player_id]['last_pos'][0]), (game_y - self.data[player_id]['last_pos'][1])))
+                # self.data[player_id]['vel'] = (game_x - self.data[player_id]['last_pos'][0]), (game_y - self.data[player_id]['last_pos'][1])
             else:
-                self.data[player_id]['vel'] = (0, 0)
+                self.data[player_id]['vel'].append((0, 0))
+                # self.data[player_id]['vel'] = (0, 0)
 
     def start_capture(self):
         threading.Thread(target=self.get_image).start()
@@ -117,6 +131,12 @@ class VideoCapture:
 
     @property
     def vel(self):
+        p1_vel = (sum([x[0] for x in self.data[self.player.player_id]['vel']])/len(self.data[self.player.player_id]['vel']), sum([x[1] for x in self.data[self.player.player_id]['vel']])/len(self.data[self.player.player_id]['vel']))
+        # print self.data[self.player.player_id]['vel'], self.data[self.player2.player_id]['vel']
         if self.player2:
-            return self.data[self.player.player_id]['vel'], self.data[self.player2.player_id]['vel']
-        return self.data[self.player.player_id]['vel']
+            p2_vel = (sum([x[0] for x in self.data[self.player2.player_id]['vel']])/len(self.data[self.player2.player_id]['vel']), sum([x[1] for x in self.data[self.player2.player_id]['vel']])/len(self.data[self.player2.player_id]['vel']))
+            # print p1_vel, p2_vel
+            # return self.data[self.player.player_id]['vel'], self.data[self.player2.player_id]['vel']
+            return p1_vel, p2_vel
+        return p1_vel
+        # return self.data[self.player.player_id]['vel']
